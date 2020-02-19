549e5e6827dddf9988cb6daf47f14418f3d88173
sumiradio <110142594+sumiradio@users.noreply.github.com>
2020-02-19 09:19:27 +0000

    msm: mdss: Notify userspace of idle screen case

--- client/components/booking.js
+++ client/components/booking.js
@@ -1,3 +1,21 @@
 import React, {Component} from 'react'
 import {fetchBookings} from '../store/bookingReducer'
-import {connect} from 'react-r
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+
+export class Booking extends Component {
+  componentDidMount() {
+    this.props.fetchBookings()
+  }
+  render() {
+    const {bookings, loading, userId} = this.props
+
+    if (loading) return <div>Loading...</div>
+    let history
+
+    if (
+      bookings.filter(booking => booking.mentorId === userId) ||
+      bookings.filter(booking => booking.menteeId === userId)
+    ) {
+      history = bookings.filter(
+        booking => booking.mentorId === userId || boo
