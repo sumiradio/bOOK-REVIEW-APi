1ab182445b0c0c1b77c6c0280c6a7f905b28601f
sumiradio <110142594+sumiradio@users.noreply.github.com>
2020-03-31 08:41:30 +0000

    ux

--- client/components/index.js
+++ client/components/index.js
@@ -1,2 +1,13 @@
 /**
- * `components/index.js` exists simply as a 'central expo
+ * `components/index.js` exists simply as a 'central export' for our components.
+ * This way, we can import all of our components from the same place, rather than
+ * having to figure out which file they belong to!
+ */
+export {default as NavbarMenu} from './navbar'
+export {default as UserHome} from './user-home'
+export {default as UserProfile} from './user-profile'
+export {Login, Signup} from './auth-form'
+export {default as Dashboard} from './dashboard'
+export {default as Booking} from './booking'
+export {default as Search} from './search'
+export {default as Welcome} from 
