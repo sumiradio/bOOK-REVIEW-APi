f1b85acf37c5d3f64db75b9eb038fc05d465cb2a
sumiradio <110142594+sumiradio@users.noreply.github.com>
2020-10-07 21:37:21 +0000

    Bump version to 1.0.2.

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,4 +6,24 @@ import {logout} from '../store'
 import {Login} from '../components'
 import {Navbar, Button, Dropdown, Icon} from 'react-bulma-components/full'
 
-class NavbarMenu exten
+class NavbarMenu extends Component {
+  state = {
+    active: false
+  }
+
+  navToggleClick = () => {
+    const {active} = this.state
+    this.setState({active: !active})
+  }
+
+  render() {
+    const {handleClick, isLoggedIn} = this.props
+    return (
+      <Navbar role="navigation" aria-label="main navigation">
+        <a href={isLoggedIn ? '/home' : '/'} alt="Main Page">
+          <Navbar.Brand>
+            <img src={require('./elevate.png')} width="160" height="160" />
+          </Navbar.Brand>
+        </a>
+
+        <div className="Nav_mobile"
