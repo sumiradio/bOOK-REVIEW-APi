c1f5ecc9503ab9faf363a83170e82cc644bb8d71
sumiradio <110142594+sumiradio@users.noreply.github.com>
2020-12-02 14:17:15 +0000

    Linux 3.0.86

--- client/components/signup-form.js
+++ client/components/signup-form.js
@@ -2,4 +2,25 @@ import React from 'react'
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
 import {signup} from '../store'
-import {Link} from 'react-router-dom
+import {Link} from 'react-router-dom'
+
+/**
+ * COMPONENT
+ */
+const AuthForm = props => {
+  const {name, displayName, handleSubmit, error} = props
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} name={name}>
+        <div>
+          <label htmlFor="status">
+            <small>Are you a Mentor or a Mentee?</small>
+          </label>
+          <input type="checkbox" name="mentorMentee" value="mentor" /> Mentor
+          <input type="checkbox" name="mentorMentee" value="mentee" /> Mentee
+        </div>
+
+        <div>
+          <label htmlFor="skill">
+     
