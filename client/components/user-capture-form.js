1be8178fcb788169a982a5d1a3f6ebcdba795066
sumiradio <110142594+sumiradio@users.noreply.github.com>
2020-12-28 15:37:52 +0000

    Fix deprecations

--- client/components/user-capture-form.js
+++ client/components/user-capture-form.js
@@ -2,4 +2,26 @@ import React from 'react'
 import axios from 'axios'
 import {SkillsListName, SkillsListBox} from './skills-list'
 import {connect} from 'react-redux'
-import {updateUser}
+import {updateUser} from '../store/user'
+import history from '../history'
+import {Columns, Table} from 'react-bulma-components/full'
+
+class UserCaptureForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      skills: [],
+      form: {
+        gender: '',
+        skillsInterestedIn: [],
+        currentCompany: '',
+        currentPosition: '',
+        bio: '',
+        currentSkills: [],
+        calendlyUsername: ''
+      }
+    }
+  }
+
+  async componentDidMount() {
+    const {data} = await axios.get('/
