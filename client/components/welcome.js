0dc3588701caddc9fc1dfdb22da9844573c14574
sumiradio <110142594+sumiradio@users.noreply.github.com>
2021-02-17 12:24:22 +0000

    testimonials updated via TUNE

--- client/components/welcome.js
+++ client/components/welcome.js
@@ -7,4 +7,17 @@ const Welcome = () => {
     <Hero size="fullheight" className="aboutSection">
       <Hero.Head renderAs="header">
         <Heading className="hero-title" size={1}>
-          Welcome to Elevate{
+          Welcome to Elevate{' '}
+        </Heading>
+        <Heading className="subtitle" size={3}>
+          A reverse mentoring app.
+        </Heading>
+      </Hero.Head>
+      <br />
+      <Hero.Body>
+        <div className="columns">
+          <div className="column">
+            <Heading size={5}>What is Reverse Mentoring?</Heading>
+            It is a career development practice that helps businesses mine
+            certain insights or information of the less experienced, often
+            younger employ
