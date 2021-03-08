af23d1c13f6a8765122bdd19e7c6ac158d6285e0
sumiradio <110142594+sumiradio@users.noreply.github.com>
2021-03-08 08:46:55 +0000

    Solve

--- client/components/welcome.js
+++ client/components/welcome.js
@@ -20,4 +20,18 @@ const Welcome = () => {
             <Heading size={5}>What is Reverse Mentoring?</Heading>
             It is a career development practice that helps businesses mine
             certain insights or information of the less experienced, often
-            younger employ
+            younger employees for senior executives
+            <p>
+              <a
+                href="https://grasshopperherder.com/reverse-mentoring-what-millennials-can-teach-executives-and-senior-managers"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="button learn-buttons is-danger"
+              >
+                Learn more
+              </a>
+            </p>
+          </div>
+          <div className="column">
+            <Heading size={5}>
+              How can we close the gender
