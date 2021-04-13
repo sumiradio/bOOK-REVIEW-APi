02c5a7b1b2057df67c458662665e69edb4c29e0e
sumiradio <110142594+sumiradio@users.noreply.github.com>
2021-04-13 13:18:07 +0000

    add components

--- client/store/index.js
+++ client/store/index.js
@@ -1,2 +1,20 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux'
-import {createLogge
+import {createLogger} from 'redux-logger'
+import thunkMiddleware from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import user from './user'
+import profileReducer from './profiles'
+
+// import mentorReducer from './mentorReducer'
+// import menteeReducer from './menteeReducer'
+import bookingReducer from './bookingReducer'
+import skillsReducer from './skillsListReducer'
+
+const reducer = combineReducers({
+  user,
+  profileReducer,
+  // mentorReducer,
+  // menteeReducer,
+  bookingReducer,
+  skillsReducer
+}
