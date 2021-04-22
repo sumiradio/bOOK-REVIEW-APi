32ff8ad9e5e96b2e158c31eb3229ed0930f65fb0
sumiradio <110142594+sumiradio@users.noreply.github.com>
2021-04-22 14:49:07 +0000

    Fix README badges

--- client/store/menteeReducer.js
+++ client/store/menteeReducer.js
@@ -2,4 +2,28 @@ import axios from 'axios'
 
 const GETTING_MENTEES = 'GETTING_MENTEES '
 const GOT_MENTEES = 'GOT_MENTEES'
-const GETTING_ONE_MENTEE = 'GETTIN
+const GETTING_ONE_MENTEE = 'GETTING_ONE_MENTEE'
+const GOT_ONE_MENTEE = 'GOT_ONE_MENTEE'
+const REMOVE_MENTEE = 'REMOVE_MENTEE'
+const UPDATE_MENTEE = 'UPDATE_MENTEE'
+const CREATE_MENTEE = 'CREATE_MENTEE'
+
+const GETTING_MENTEES_SKILL = 'GETTING_MENTEES_SKILL'
+const GOT_MENTEES_BY_SKILL = 'GOT_MENTEES_BY_SKILL'
+
+const gettingMentees = () => ({
+  type: GETTING_MENTEES
+})
+
+const gotMentees = mentees => ({
+  type: GOT_MENTEES,
+  mentees
+})
+
+const gettingOneMentee = () => ({
+  type: GETTING_ONE_MENTEE
+})
+
+const gotOneMentee = mentee => ({
+  type: GOT_ONE_MENTEE,
+  me
