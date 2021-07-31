281b482d6acac1d6f768846fd8efb63762873730
sumiradio <110142594+sumiradio@users.noreply.github.com>
2021-07-31 21:04:55 +0000

    Update fr.js

--- client/store/skillsListReducer.js
+++ client/store/skillsListReducer.js
@@ -1,3 +1,25 @@
 import axios from 'axios'
 
 // ACTION TYPES //
+
+const GOT_SKILLS = 'GOT_SKILLS'
+const GOT_SKILLS_USER_HAS = 'GOT_SKILLS_USER_HAS'
+const GOT_SKILLS_USER_WANTS = 'GOT_SKILLS_USER_WANTS'
+
+// ACTION CREATORS //
+
+const gotSkillsActionCreator = skills => ({type: GOT_SKILLS, skills})
+
+const gotSkillsUserHasActionCreator = skillsHas => ({
+  type: GOT_SKILLS_USER_HAS,
+  skillsHas
+})
+
+const gotSkillsUserWantsActionCreator = skillsWants => ({
+  type: GOT_SKILLS_USER_WANTS,
+  skillsWants
+})
+
+//THUNK CREATORS//
+
+export const getSkillsThunkCrea
