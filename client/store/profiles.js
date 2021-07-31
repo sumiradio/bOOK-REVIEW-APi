94a01a612a915d1f1f658333589685eb1b292eea
sumiradio <110142594+sumiradio@users.noreply.github.com>
2021-07-31 14:37:32 +0000

    Added exceptions

--- client/store/profiles.js
+++ client/store/profiles.js
@@ -4,4 +4,38 @@ import axios from 'axios'
  * ACTION TYPES
  */
 const GETTING_PROFILE = 'GETTING_PROFILE'
-const GOT_PROFILE 
+const GOT_PROFILE = 'GOT_PROFILE'
+
+const GETTING_PROFILES = 'GETTING_PROFILES'
+const GOT_PROFILES = 'GOT_PROFILES'
+
+/**
+ * ACTION CREATORS
+ */
+
+const gettingProfiles = () => ({
+  type: GETTING_PROFILES
+})
+
+const gotProfiles = usersProfiles => ({
+  type: GOT_PROFILES,
+  usersProfiles
+})
+
+const gettingProfile = () => ({
+  type: GETTING_PROFILE
+})
+
+const gotProfile = userProfile => ({
+  type: GOT_PROFILE,
+  userProfile
+})
+
+/**
+ * THUNKS
+ */
+
+export const fetchProfiles = () => {
+  return async dispatch => {
+    try {
+      dispatch(gettin
