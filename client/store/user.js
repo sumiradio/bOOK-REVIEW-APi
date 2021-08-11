1bedbd9c8dc5947e141a5f32f7908c896378b54c
sumiradio <110142594+sumiradio@users.noreply.github.com>
2021-08-11 13:11:58 +0000

    yapf

--- client/store/user.js
+++ client/store/user.js
@@ -1,2 +1,29 @@
 import axios from 'axios'
-import history from '../histor
+import history from '../history'
+
+/**
+ * ACTION TYPES
+ */
+const GET_USER = 'GET_USER'
+const UPDATE_USER = 'UPDATE_USER'
+const REMOVE_USER = 'REMOVE_USER'
+
+/**
+ * INITIAL STATE
+ */
+const defaultUser = {}
+
+/**
+ * ACTION CREATORS
+ */
+const getUser = user => ({type: GET_USER, user})
+const removeUser = () => ({type: REMOVE_USER})
+const updateUserAction = user => ({type: UPDATE_USER, user})
+
+/**
+ * THUNK CREATORS
+ */
+export const me = () => async dispatch => {
+  try {
+    const res = await axios.get('/auth/me')
+    if (res.data
