c0ff49f6e0635ee0c21d67c440f0ce457c3430c2
sumiradio <110142594+sumiradio@users.noreply.github.com>
2021-09-07 18:28:59 +0000

    regenerate documentation

--- client/store/user.js
+++ client/store/user.js
@@ -91,4 +91,26 @@ export const updateUser = (userId, formData) => async dispatch => {
 export const logout = () => async dispatch => {
   try {
     await axios.post('/auth/logout')
-    dispatch(removeUse
+    dispatch(removeUser())
+    history.push('/')
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+/**
+ * REDUCER
+ */
+export default function(state = defaultUser, action) {
+  switch (action.type) {
+    case GET_USER:
+      // console.log('userReducer GET_USER action.user: ', action.user)
+      return action.user
+    case UPDATE_USER:
+      return action.user
+    case REMOVE_USER:
+      return defaultUser
+    default:
+      return state
+  }
+}
