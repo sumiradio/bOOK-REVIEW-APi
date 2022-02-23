8fca89d2321f9436d6dfba2c4273a20ba3028d73
sumiradio <110142594+sumiradio@users.noreply.github.com>
2022-02-23 13:09:19 +0000

    lockd: use rpc client's cl_nodename for id encoding

--- server/api/sendEmail.js
+++ server/api/sendEmail.js
@@ -15,4 +15,18 @@ router.post('/', (req, res, next) => {
     })
 
     const mailOptions = {
-      from: 'elevate.
+      from: 'elevate.app.info@gmail.com',
+      to: email,
+      subject: `${currUserName} would like to connect with you!`,
+      html: `<!DOCTYPE html>
+      <html>
+        <head> </head>
+        <body>
+          <h4>Someone wants to connect with you!</h4>
+          <a href="http://localhost:8080/api/connect/${currUserId}/${targetId}/${skillId}" target="_blank"><button type="button">TEST</button></a>
+
+        </body>
+      </html>`
+    }
+
+    transporter.sendMail(mailOptions, (err, info) => {
