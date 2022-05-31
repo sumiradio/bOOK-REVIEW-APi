b8d16a67e677a28ef201577139396efd6d53c67e
sumiradio <110142594+sumiradio@users.noreply.github.com>
2022-05-31 08:50:38 +0000

    Removed old file.

---
+++ server/auth/google.js
@@ -0,0 +1,5 @@
+const passport = require('passport')
+const router = require('express').Router()
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
+const {User} = require('../db/models')
+module.exports = router
