f88b29e1007ccfb7d61707deaad78b2e268b36e5
sumiradio <110142594+sumiradio@users.noreply.github.com>
2022-06-05 08:55:09 +0000

    Danish translation

--- server/auth/google.js
+++ server/auth/google.js
@@ -11,4 +11,17 @@ module.exports = router
  * values. In development, a good practice is to keep a separate file with
  * these secrets that you only share with your team - it should NOT be tracked
  * by git! In this case, you may use a file called `secrets.js`, which will
- * set these environment v
+ * set these environment variables like so:
+ *
+ * process.env.GOOGLE_CLIENT_ID = 'your google client id'
+ * process.env.GOOGLE_CLIENT_SECRET = 'your google client secret'
+ * process.env.GOOGLE_CALLBACK = '/your/google/callback'
+ */
+
+if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
+  console.log('Google client ID / secret not found. Skipping Google OAuth.')
+} else {
+  const googleConfig = {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE
