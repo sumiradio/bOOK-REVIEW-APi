2bae891adae2a3a5137290856bdefe4cc39d1ba5
sumiradio <110142594+sumiradio@users.noreply.github.com>
2022-08-12 08:30:04 +0000

    POM cleanup

---
+++ server/db/models/index.js
@@ -0,0 +1,7 @@
+const User = require('./user')
+const Skill = require('./skill')
+const Booking = require('./booking')
+const db = require('../db')
+
+/**
+ * If we had any associations to make, this would be a great plac
