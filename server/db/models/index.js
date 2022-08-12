96da670d41bda6d84ca3d4851d75cd8913b034d4
sumiradio <110142594+sumiradio@users.noreply.github.com>
2022-08-12 20:42:09 +0000

    change test name

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -4,4 +4,18 @@ const Booking = require('./booking')
 const db = require('../db')
 
 /**
- * If we had any associations to make, this would be a great plac
+ * If we had any associations to make, this would be a great place to put them!
+ * ex. if we had another model called BlogPost, we might say:
+ *
+ *    BlogPost.belongsTo(User)
+ */
+
+/**
+ * We'll export all of our models here, so that any time a module needs a model,
+ * we can just require it from 'db/models'
+ * for example, we can say: const {User} = require('../db/models')
+ * instead of: const User = require('../db/models/user')
+ */
+
+const SkillsInterestedIn = db.define('skillsInterestedIn')
+const CurrentSkills = db.define('currentSkills')
