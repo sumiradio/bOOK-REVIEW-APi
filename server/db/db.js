5ce873499dc785dfacefe679353778c65c4bd6a2
sumiradio <110142594+sumiradio@users.noreply.github.com>
2022-08-12 07:22:07 +0000

    init update

--- server/db/db.js
+++ server/db/db.js
@@ -23,4 +23,15 @@ if (process.env.DATABASE_URL) {
 }
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || `p
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  config
+)
+
+// This is a global Mocha hook used for resource cleanup.
+// Otherwise, Mocha v4+ does not exit after tests.
+if (process.env.NODE_ENV === 'test') {
+  after('close database connection', () => db.close())
+}
+
+// Exports
+module.exports = db
---
+++ server/db/models/booking.js
@@ -0,0 +1,4 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Booking = 
