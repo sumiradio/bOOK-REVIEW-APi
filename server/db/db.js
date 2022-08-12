59e7abcd46fdac2ee05d3a00132860b8e77f7130
sumiradio <110142594+sumiradio@users.noreply.github.com>
2022-08-12 06:00:01 +0000

    Fix coding conventions violations.

--- server/db/db.js
+++ server/db/db.js
@@ -1,1 +1,26 @@
-// 
+// Imports
+const Sequelize = require('sequelize')
+
+const pkg = require('../../package.json')
+
+// Initializations
+const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
+let config
+
+if (process.env.DATABASE_URL) {
+  config = {
+    logging: false,
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+} else {
+  config = {logging: false}
+}
+
+const db = new Sequelize(
+  process.env.DATABASE_URL || `p
