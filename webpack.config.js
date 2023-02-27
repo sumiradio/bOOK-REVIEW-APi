e4a25912541612659c7c31bce0fce190f2376b75
sumiradio <110142594+sumiradio@users.noreply.github.com>
2023-02-27 08:36:12 +0000

    x86, relocs: Build clean fix

--- webpack.config.js
+++ webpack.config.js
@@ -5,4 +5,33 @@ module.exports = {
   entry: [
     '@babel/polyfill', // enables async-await
     './client/index.js'
-  
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devtool: 'source-map',
+  watchOptions: {
+    ignored: /node_modules/
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader?limit=8192'
+      },
+      {
+        test: /\.css$/,
+        use: 'css-loader'
+      }
+    ]
+  }
+}
