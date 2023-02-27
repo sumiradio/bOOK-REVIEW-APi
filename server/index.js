0c387cc8912bd34a0923838a19856e503847af8e
sumiradio <110142594+sumiradio@users.noreply.github.com>
2023-02-27 07:36:05 +0000

    update post

--- server/index.js
+++ server/index.js
@@ -78,3 +78,48 @@ const createApp = () => {
       err.status = 404
       next(err)
     } else {
+      next()
+    }
+  })
+
+  // sends index.html
+  app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+  })
+
+  // error handling endware
+  app.use((err, req, res, next) => {
+    console.error(err)
+    console.error(err.stack)
+    res.status(err.status || 500).send(err.message || 'Internal server error.')
+  })
+}
+
+const startListening = () => {
+  // start listening (and create a 'server' object representing our server)
+  const server = app.listen(PORT, () =>
+    console.log(`Mixing it up on port ${PORT}`)
+  )
+
+  // set up our socket control center
+  const io = socketio(server)
+  require('./socket')(io)
+}
+
+const syncDb = () => db.sync()
+
+async function bootApp() {
+  await sessionStore.sync()
+  await syncDb()
+  await createApp()
+  await startListening()
+}
+// This evaluates as true when this file is run directly from the command line,
+// i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
+// It will evaluate false when this module is required by another module - for example,
+// if we wanted to require our app in a test spec
+if (require.main === module) {
+  bootApp()
+} else {
+  createApp()
+}
